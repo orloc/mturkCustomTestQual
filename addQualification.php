1c0f1c8dd140c45c37458a7105ab936dbfc73d3a
Grant Tepper <grant.tepper@gmail.com>
2014-03-11 01:09:09 -0400

    added test schema files

--- addQualification.php
+++ addQualification.php
@@ -6,6 +6,15 @@ use Guzzle\Http\Client;
 
 $conf = require __DIR__.'/config/parameters.php';
 
+$qual = __DIR__.'/resources/qualifcations/questionairre.xml';
+$answ = __DIR__.'/resources/qualifcations/answers.xml';
+
+// grab the test qualifcation 
+$qualXml = simplexml_load_file($qual);
+$answXml = simplexml_load_file($answ);
+
+
+// prep our http client and grab some things
 $client = new Client();
 $url = $conf['url'];
 $key = $conf['key'];
@@ -13,6 +22,8 @@ $key = $conf['key'];
 unset($conf['key']);
 unset($conf['url']);
 
+
+// -- Generate our request
 $request = $client->get($url, [], [ 'debug' => true ]);
 $query = $request->getQuery();
 
@@ -21,25 +32,33 @@ $creds = generateSig($conf, $key);
 $additonalParams = [
     'Signature' => $creds[0],
     'Timestamp' => $creds[1],
+    'Test' => trim($qualXml->asXml(), " \t\n\r"),
+    'AnswerKey' => (string)$answXml->asXml()
 ];
 
+var_dump(strlen($additonalParams['Test']), strlen($additonalParams['AnswerKey']));die;
 $params = array_merge($conf, $additonalParams);
 ksort($params);
-
 
 foreach ($params as $k => $v) {
     $query->add($k, $v);
 }
 
+ 
+// Hope it works?
 try { 
-$response = $request->send();
-var_dump($response->getBody(true));
+    $response = $request->send();
+    print "HERE";
+    print "success";
+} catch (\Guzzle\Http\Exception\CurlException  $e) {
+    var_dump($e);
 } catch (\Exception $e) {
-    var_dump($e->getResponse()->getBody(true));
+    print "general exception";
+    var_dump($e->getMessage());
 }
-/**
- *  
- */
+
+
+// =====================================================
 function generateSig(array $conf, $key) { 
 
     $ts = getDateTime();
@@ -47,7 +66,6 @@ function generateSig(array $conf, $key) {
     $hmac = hash_hmac('sha1', $hmacString, $key, true);
 
     $sig = base64_encode($hmac);
-
 
     return [$sig, $ts];
 }
